8dcb2692e4128b3bbefdcaa333e823ba400154f4
alan-zuo <alan666zuo@163.com>
2017-05-04 14:31:29 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,36 @@
+#
+# Redis的安装脚本
+#
+
+# 参数说明
+# $1 要安装的目录，必选
+# $2 Redis端口号，必选
+
+if [ $# != 2 ]; then
+    echo "ERROR: Param error"
+    echo "Usage: $0 PREFIX [PORT]"
+    exit
+fi
+
+prefix=$1
+port=$2
+
+tar -zxvf redis-3.2.8.tar.gz
+cd redis-3.2.8
+make
+make PREFIX=$prefix install
+rm -rf redis-3.0.3
+cd ..
+cp redis.conf $prefix/redis.conf.template
+cp start.sh stop.sh restart.sh $prefix
+
+cd $prefix
+
+mkdir data
+
+sed "s:__WORK_DIR__:$prefix:g" redis.conf.template |
+sed "s:__PORT__:$port:g" > redis.conf
+
+rm -rf redis.conf.template
+
+echo "安装完成"
