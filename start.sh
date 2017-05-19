34333a5086f7caed66e6772df0ca23f15f1ba920
alan-zuo <alan666zuo@163.com>
2017-05-19 11:21:41 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ work_dir=`pwd`
 
 $work_dir/bin/redis-server $work_dir/redis.conf
 if [ $? == 0 ]; then
-    echo "Redis启动成功"
+    echo "Redis 启动成功"
 else
-    echo "ERROR: Redis启动失败"
+    echo "ERROR: Redis 启动失败"
 fi
