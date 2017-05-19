858a6df8e689e1fdd49b4941d29cfcc294480f35
alan-zuo <alan666zuo@163.com>
2017-05-19 11:21:57 +0800

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -7,7 +7,7 @@ work_dir=`pwd`
 # 停止Cuttle
 kill `cat $work_dir/redis.pid`
 if [ $? == 0 ]; then
-    echo "Redis停止成功"
+    echo "Redis 停止成功"
 else
-    echo "ERROR: Redis停止失败"
+    echo "ERROR: Redis 停止失败"
 fi
